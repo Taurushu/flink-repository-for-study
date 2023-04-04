855c30f021ac53c1fa75306f903c40c66202e2c5
taurusJ <2125420758@qq.com>
2023-04-04 15:47:59 +0800

    DataStream source study 代码文件

---
+++ flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadKafkaSource.java
@@ -0,0 +1,24 @@
+package top.taurushu.streamSource;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class ReadKafkaSource {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("node1:9092")
+                .setTopics("input-topic")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source").print();
+        env.execute();
+    }
+}
--- flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadTextFileSource.java
+++ flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadTextFileSource.java
@@ -1,7 +1,6 @@
 package top.taurushu.streamSource;
 
 
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
