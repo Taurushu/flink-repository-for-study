78a4211ed91c4ea90efe51c1b2edf09710f58dd2
taurusJ <2125420758@qq.com>
2023-04-04 19:17:19 +0800

    更新代码

--- flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadKafkaSource.java
+++ flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadKafkaSource.java
@@ -4,6 +4,7 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class ReadKafkaSource {
@@ -17,7 +18,9 @@ public class ReadKafkaSource {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
-        env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source").print();
+        DataStreamSource<String> kafkaSource =
+                env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        kafkaSource.print();
         env.execute();
     }
 }
