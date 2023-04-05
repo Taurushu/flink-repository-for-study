9622b14badf90d6d264651b3029b38fe0455550b
taurusJ <2125420758@qq.com>
2023-04-05 22:07:40 +0800

    Write LocalFileSystem/Kafka Sink

---
+++ flink-datastream-api/src/main/java/top/taurushu/streamSink/WriteKafkaSink.java
@@ -0,0 +1,44 @@
+package top.taurushu.streamSink;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WriteKafkaSink {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("node1:9092")
+                .setTopics("input-topic")
+                .setGroupId("my-group")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStreamSource<String> kafkaSource =
+                env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        SingleOutputStreamOperator<String> returns = kafkaSource.map("舒杰"::concat).returns(Types.STRING);
+
+        KafkaSink<String> sink = KafkaSink.<String>builder()
+                .setBootstrapServers("node1:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema
+                        .builder()
+                        .setTopic("output-topic")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
+
+        returns.sinkTo(sink);
+        env.execute();
+    }
+}
