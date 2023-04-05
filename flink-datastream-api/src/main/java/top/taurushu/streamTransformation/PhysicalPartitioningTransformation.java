dc00e325343c2f3ae24ead15f0cfbe260426918b
taurusJ <2125420758@qq.com>
2023-04-05 19:39:37 +0800

    Aggregation DIYSource for scale

--- flink-datastream-api/src/main/java/top/taurushu/streamTransformation/PhysicalPartitioningTransformation.java
+++ flink-datastream-api/src/main/java/top/taurushu/streamTransformation/PhysicalPartitioningTransformation.java
@@ -1,19 +1,11 @@
 package top.taurushu.streamTransformation;
 
 
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.tuple.Tuple2;
+
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
 import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
-import org.apache.flink.util.Collector;
 import top.taurushu.streamSource.Event;
-
-import java.util.Arrays;
-
 
 public class PhysicalPartitioningTransformation {
     public static void main(String[] args) throws Exception {
