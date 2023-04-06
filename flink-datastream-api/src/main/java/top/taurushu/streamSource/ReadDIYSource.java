1582522e9a553322dc581aa46defa407d2138578
taurusJ <2125420758@qq.com>
2023-04-06 16:57:12 +0800

    Write LocalFileSystem/Kafka/Redis Sink

--- flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadDIYSource.java
+++ flink-datastream-api/src/main/java/top/taurushu/streamSource/ReadDIYSource.java
@@ -2,10 +2,6 @@ package top.taurushu.streamSource;
 
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
-
-import java.util.Date;
-import java.util.Random;
 
 public class ReadDIYSource {
     public static void main(String[] args) throws Exception {
