76bd74e693721b277ed77fc937b926fae84fbc75
taurusJ <2125420758@qq.com>
2023-04-07 17:30:10 +0800

    修改代码，使用lombok

--- flink-time-wartemark-window/src/main/java/top/taurushu/pojo/Event.java
+++ flink-time-wartemark-window/src/main/java/top/taurushu/pojo/Event.java
@@ -1,40 +1,16 @@
 package top.taurushu.pojo;
 
+import lombok.Data;
+
 import java.sql.Timestamp;
 
+@Data
 public class Event {
 
     private String name;
     private String uri;
     private Long time;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public Long getTime() {
-        return time;
-    }
-
-    public void setTime(Long time) {
-        this.time = time;
-    }
-
-    public Event() {
-    }
 
     public Event(String value) {
         this(value.split(",")[0], value.split(",")[1], Long.valueOf(value.split(",")[2]));
@@ -45,12 +21,7 @@ public class Event {
         this.uri = uri;
         this.time = time;
     }
-
-
-    public String toCsv() {
-        return name + ',' + uri + "," + time;
-    }
-
+    
     @Override
     public String toString() {
         return "Event{" +
