a2d6e0a6ab19dcca5c134a3be5e0b9d1a1032377
taurusJ <2125420758@qq.com>
2023-04-07 12:59:07 +0800

    封装了从kafka读取数据以及Kafka写入数据的的模式

--- flink-datastream-api/src/main/java/top/taurushu/streamSink/WriteKafkaSink.java
+++ flink-datastream-api/src/main/java/top/taurushu/streamSink/WriteKafkaSink.java
@@ -1,38 +1,29 @@
 package top.taurushu.streamSink;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import top.taurushu.streamSource.DiyParallelSourceFunc;
+import top.taurushu.streamSource.Event;
 
 public class WriteKafkaSink {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        KafkaSource<String> source = KafkaSource.<String>builder()
-                .setBootstrapServers("node1:9092")
-                .setTopics("input-topic")
-                .setGroupId("my-group")
-                .setStartingOffsets(OffsetsInitializer.earliest())
-                .setValueOnlyDeserializer(new SimpleStringSchema())
-                .build();
+        DataStreamSource<Event> source = env.addSource(new DiyParallelSourceFunc()).setParallelism(2);
 
-        DataStreamSource<String> kafkaSource =
-                env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-        SingleOutputStreamOperator<String> returns = kafkaSource.map("舒杰"::concat).returns(Types.STRING);
+        SingleOutputStreamOperator<String> returns = source.map(Event::toCsv).returns(Types.STRING);
 
         KafkaSink<String> sink = KafkaSink.<String>builder()
                 .setBootstrapServers("node1:9092")
                 .setRecordSerializer(
                         KafkaRecordSerializationSchema
                                 .builder()
-                                .setTopic("output-topic")
+                                .setTopic("flink-generate-topic")
                                 .setValueSerializationSchema(new SimpleStringSchema())
                                 .build()
                 )
