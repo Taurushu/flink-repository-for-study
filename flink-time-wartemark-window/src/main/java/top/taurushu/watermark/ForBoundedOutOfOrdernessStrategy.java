931075c08ca9bcdf726da728d93945595cdcaf11
taurusJ <2125420758@qq.com>
2023-04-08 08:35:08 +0800

    更改为携带计数窗口的统计

--- flink-time-wartemark-window/src/main/java/top/taurushu/watermark/ForBoundedOutOfOrdernessStrategy.java
+++ flink-time-wartemark-window/src/main/java/top/taurushu/watermark/ForBoundedOutOfOrdernessStrategy.java
@@ -2,6 +2,7 @@ package top.taurushu.watermark;
 
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import top.taurushu.pojo.Event;
 import top.taurushu.utils.FromKafkaSource;
@@ -14,10 +15,15 @@ public class ForBoundedOutOfOrdernessStrategy {
 
         Function<SingleOutputStreamOperator<Event>, Void> function = kafkaSource -> {
             kafkaSource.assignTimestampsAndWatermarks(
-                    WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofMillis(120)).withTimestampAssigner(
-                            (SerializableTimestampAssigner<Event>) (element, recordTimestamp) -> element.getTime()
-                    )
-            ).print();
+                            WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ofMillis(120)).withTimestampAssigner(
+                                    (SerializableTimestampAssigner<Event>) (element, recordTimestamp) -> element.getTime()
+                            )
+                    ).map((Event e) -> 1L)
+                    .keyBy(key -> "default")
+                    .countWindow(200)
+//                    .window(TumblingEventTimeWindows.of(Time.milliseconds(1000),Time.milliseconds(500)))
+                    .reduce((ReduceFunction<Long>) Long::sum)
+                    .print();
             return null;
         };
         FromKafkaSource.executeFromKafkaSource(function);
